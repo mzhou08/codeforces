7edc4b55f83b4cfbd6f4c6bfd929504c08d0479c
Michael Zhou <mhzhou@andrew.cmu.edu>
2022-08-10 11:34:08 -0400

    solved 1713-A: Traveling Salesman Problem

---
+++ 1713-A.py
@@ -0,0 +1,16 @@
+def find_greatest_rectangle_dimensions() -> tuple[int, int]:
+    num_points = int(input())
+    xs = [0]
+    ys = [0]
+    for _ in range(num_points):
+        x, y = [int(elem) for elem in input().split(' ')]
+        xs.append(x)
+        ys.append(y)
+
+    return (max(xs) - min(xs), max(ys) - min(ys))
+
+num_cases = int(input())
+
+for _ in range(num_cases):
+    x_dim, y_dim = find_greatest_rectangle_dimensions()
+    print(f"{2 * (x_dim + y_dim)}")

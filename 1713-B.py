fd23fd24ff0902b9a0e2ee13d1e40c64ca4e5bea
Michael Zhou <mhzhou@andrew.cmu.edu>
2022-08-12 19:36:03 -0400

    solved 1713-B: Optimal Reduction

---
+++ 1713-B.py
@@ -0,0 +1,21 @@
+def is_optimal_reduction():
+    n = int(input())
+    arr: list[int] = [int(elem) for elem in input().split()]
+
+    curr_min: int = arr[0]
+    new_min: bool = False
+
+    for elem in arr:
+        if elem < curr_min:
+            curr_min = elem
+            new_min = True
+        elif new_min and elem > curr_min:
+            return False
+        else:
+            curr_min = elem
+    return True
+
+
+t = int(input())
+for _ in range(t):
+    print("YES") if is_optimal_reduction() else print("NO")

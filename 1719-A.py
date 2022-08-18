f7d9589dccb2e8070788cc60331d4ff9426a6938
Michael Zhou <mhzhou@andrew.cmu.edu>
2022-08-17 21:44:30 -0400

    contest submissions for 1719

---
+++ 1719-A.py
@@ -0,0 +1,9 @@
+t = int(input())
+
+for _ in range(t):
+    m, n = [int(x) for x in input().split()]
+
+    if m % 2 == n % 2:
+        print("Tonya")
+    else:
+        print("Burenka")
---
+++ 1719-B.py
@@ -0,0 +1,23 @@
+t = int(input())
+
+for _ in range(t):
+    n, k = [int(x) for x in input().split()]
+
+    if k % 4 == 0:
+        print("NO")
+
+    elif k % 2 == 0: # is even but not multiple of 4
+        print("YES")
+        for i in range(1, (n // 2) + 1):
+            if i % 2 == 0: # means that 2 * i is div by 4
+                print(f"{(2 * i) - 1} {2 * i}")
+            else:
+                print(f"{2 * i} {(2 * i) - 1}")
+
+    else: # k is odd
+        print("YES")
+        for i in range(1, (n // 2) + 1):
+            print(f"{(2 * i) - 1} {2 * i}")
+
+
+        

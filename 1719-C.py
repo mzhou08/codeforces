0ddc66f32da9e89bfbf0c047fccaecc3820386a5
Michael Zhou <mhzhou@andrew.cmu.edu>
2022-08-17 22:31:57 -0400

    changed to cleaned format, still slight bug

--- 1719-C.py
+++ 1719-C.py
@@ -1,29 +1,3 @@
-def generate_wins_by_strength(
-    n: int,
-) -> tuple[dict[int, int], int]:
-    # returns a tuple of:
-    #   - wins by strength
-    #   - number of rounds to get to cycle
-
-    rounds_passed = 0
-
-    wins: dict[int, int] = {i: 0 for i in range(1, n + 1)}
-    # wins keyed by strength
-
-    while arr[0] != n: # while the strongest fighter is not first
-
-        if arr[0] > arr[1]:
-            wins[arr[0]] += 1 # stronger fighter gets a win
-            loser = arr.pop(1)
-        else:
-            wins[arr[1]] += 1 # stronger fighter gets a win
-            loser = arr.pop(0)
-
-        arr.append(loser)
-        rounds_passed += 1
-
-    return (wins, rounds_passed)
-
 t = int(input())
 
 for _ in range(t):
@@ -33,8 +7,6 @@ for _ in range(t):
 
     initial_arr = list(arr)
 
-    wins, rounds_until_cycle = generate_wins_by_strength(n)
-
     to_print: list[int] = []
 
     for _ in range(q):
@@ -42,23 +14,27 @@ for _ in range(t):
 
         strength = initial_arr[fighter - 1]
 
-        if k < rounds_until_cycle:
-            if list(filter(lambda x: x > strength, initial_arr[:fighter])):
-                num_wins = 0
+        if k < fighter - 2:
+            to_print.append(0)
+
+        elif list(filter(lambda x: x > strength, initial_arr[:fighter])):
+            to_print.append(0)
+
+        elif strength == n: # if strongest one, it's the number of elems
+            if fighter <= 2:
+                to_print.append(k)
             else:
-                nearest_count = 0
-                for j in initial_arr[fighter:]:
-                    nearest_count += 1
-                    if j > strength:
-                        break
+                to_print.append(k - fighter + 2)
+
+        else:
+            nearest_count = -1
+            for j in initial_arr[fighter - 1:]:
+                if j > strength:
+                    break
+                nearest_count += 1
                 num_wins = min(nearest_count, k)
             to_print.append(num_wins)
             continue
-
-        if strength == n:
-            to_print.append(wins[strength] + k - rounds_until_cycle)
-        else:
-            to_print.append(wins[strength])
 
     print(*to_print, sep="\n")
 

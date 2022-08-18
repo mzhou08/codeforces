3426720737a389ef8bb9720ad0e3ee0dd47b3749
Michael Zhou <mhzhou@andrew.cmu.edu>
2022-08-17 22:51:08 -0400

    solved 1712-B by removing the attempt at memoizing

--- 1712-B.py
+++ 1712-B.py
@@ -1,14 +1,20 @@
 t = int(input())
 
-memos = {0: [], 1: [1]}
+# memos = {0: [], 1: [1]}
 
-def get_memo(n, acc) -> list[int]:
-    if n not in memos:
-        res =get_memo(n - 2))
-        prefix.extend([n, n - 1])
-        memos[n] = prefix
-    return memos[n]
+# def create_memo(n, acc) -> list[int]:
+#     if n not in memos:
+#         return create_memo(n - 2)
+#         acc.extend([n, n - 1])
+#     return acc
 
 for _ in range(t):
     n = int(input())
-    print(*get_memo(n))
+    if n % 2 == 0:
+        for i in range(n // 2):
+            print(f"{(2 * i) + 2} {(2 * i) + 1}", end=" ")
+    else:
+        print("1", end=" ")
+        for i in range(n // 2):
+            print(f"{(2 * i) + 3} {(2 * i) + 2}", end=" ")
+    print("")
